27cff67006d9c6ce22161ca483c1c609247fa178
Adam <24621027+WhiteDopeOnPunk@users.noreply.github.com>
2022-12-22 05:00:59 -0500

    swap deck

--- src/deck.py
+++ src/deck.py
@@ -11,8 +11,8 @@ class Deck:
         n_decks = int(n_decks)
         n_decks = 8 if n_decks > 8 else n_decks
         suits = '♠♥♦♣'
-      # cards = 'A234567890JQK'
-        cards = 'A0A0A0A0A0A0A'
+        cards = 'A234567890JQK'
+      # cards = 'A0A0A0A0A0A0A'
         while n_decks > 0:
             self.cards +=  [card + suit for card in cards for suit in suits]
             n_decks -= 1
