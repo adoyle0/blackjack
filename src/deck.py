c6161affdb693d11b41364c50db995424e2dbeca
Adam <24621027+WhiteDopeOnPunk@users.noreply.github.com>
2022-12-27 17:58:49 -0500

    useless

--- src/deck.py
+++ src/deck.py
@@ -12,7 +12,6 @@ class Deck:
         self.cards = []
         n_decks = 1 if not n_decks or n_decks not in '12345678' else n_decks
         n_decks = int(n_decks)
-        n_decks = 8 if n_decks > 8 else n_decks
         suits = '♠♥♦♣'
         cards = 'A234567890JQK'
 
